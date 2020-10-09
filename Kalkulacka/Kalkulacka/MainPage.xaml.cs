baa2482e6ff5d7ace80bcd2c9e34310d305fe409
Jan Kudlacek <jkudlacek061@gmail.com>
2020-10-09 21:44:36 +0200

    Added button functionality

--- Kalkulacka/Kalkulacka/MainPage.xaml.cs
+++ Kalkulacka/Kalkulacka/MainPage.xaml.cs
@@ -1,18 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using Z.Expressions;
 
 namespace Kalkulacka
 {
     public partial class MainPage : ContentPage
     {
+
         public MainPage()
         {
-            InitializeComponent();
+            InitializeComponent();  
+        }
+        
+        public string Evaluate(string input)
+        {
+            string result;
+            try
+            {
+                result = Eval.Execute<int>(input).ToString();
+            } catch
+            {
+                result = "ERR";
+            }
+            return result;
+        }
+
+        void onButtonClicked(object sender, EventArgs e)
+        {
+            string content = (sender as Button).Text.ToString();
+            switch (content)
+            {
+                case "C":
+                    resultLabel.Text = "";
+                    break;
+
+                case "⌫":
+                    string s = resultLabel.Text;
+                    if (s.Contains("="))
+                    {
+                        resultLabel.Text = "";
+                    } else
+                    {
+                        resultLabel.Text=s.Substring(0, s.Length - 1);
+                    }
+                    break;
+
+                case "=":
+                    resultLabel.Text = "=" + Evaluate(resultLabel.Text);
+                    break;
+
+                default:
+
+                    if (resultLabel.Text.Contains("="))
+                    {
+                        resultLabel.Text = "";
+                    }
+                    else
+                    {
+                        resultLabel.Text += content;
+                    }
+                    
+                    break;
+
+            }
         }
     }
 }
